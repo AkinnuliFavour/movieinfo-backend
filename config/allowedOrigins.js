ae712b775db38c1ad67acdc0a3feaeb0b74a647e
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-07-11 09:35:44 +0100

    feat (config): create neccessary config files

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://notely-frontend.vercel.app",
+    "https://k8437h-5173.csb.app",
+    "https://notely-react-frontend.vercel.app",
+  ];
+  
+  module.exports = allowedOrigins;
+  
