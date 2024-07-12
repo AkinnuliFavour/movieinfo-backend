87dcc5129c40d5403f94daed893029b19eaf00f9
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-07-12 19:46:20 +0000

    refactor: update ForumRoute to include authentication and admin middleware for get and post methods

--- routes/ForumRoute.js
+++ routes/ForumRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const forumController = require("../controllers/ForumController");
 const auth = require("../middleware/authMiddleware");
+const isAdmin = require("../middleware/isAdminMiddleware");
 
 router
   .route("/")
-  .get(forumController.getForums)
-  .post(forumController.createForum);
+  .get(auth, forumController.getForums)
+  .post(auth, isAdmin, forumController.createForum);
 
 module.exports = router;
