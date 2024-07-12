90287b2950a4a9afcfe1234e7d553cb11772df3d
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-07-12 20:02:03 +0100

    refactor: rewrite auth middleware using try/catch

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,22 +2,30 @@ const User = require("../models/User");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-const userVerification = (req, res, next) => {
+const userVerification = async (req, res, next) => {
   console.log(req.cookies);
-  const token = req.cookies.token;
-  if (!token) {
-    return res.sendStatus(403);
-  }
-  jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
-    if (err) {
-      return res.sendStatus(403);
-    } else {
-      const user = await User.findById(data.id);
-      if (user) return res.json({ status: true, user: user.username });
-      else return res.sendStatus(403);
+
+  try {
+    // Get the token from cookie
+    const token = req.cookies.token;
+
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+
+    // Find the user
+    const user = await User.findById(data.id);
+
+    if (!user) {
+      throw new Error();
     }
-  });
-  next();
+
+    // Attach the user to the request object
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "Please authenticate." });
+  }
 };
 
 module.exports = { userVerification };
