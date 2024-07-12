219072b32fd7a1f709b78b286e70b43c3543e2b7
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-07-12 19:46:31 +0000

    refactor: add authentication middleware to fetchArticle route

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const articleController = require("../controllers/articleController");
+const auth = require("../middleware/authMiddleware");
 
-router.route("/").get(articleController.fetchArticle);
+router.route("/").get(auth, articleController.fetchArticle);
 
 module.exports = router;
