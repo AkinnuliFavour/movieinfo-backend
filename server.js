799231e0cf55df03b373b749b8fb2153fc61e675
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-07-12 20:18:25 +0100

    refactor: update code for message streaming

--- server.js
+++ server.js
@@ -1,35 +1,56 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const http = require("http");
 const path = require("path");
-const { logger, logEvents } = require("./middleware/logger");
+const socketIo = require("socket.io");
+// const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const socketHandler = require("./socket/socketHandler");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
 
+const server = http.createServer(app);
+
 console.log(process.env.NODE_ENV);
 
+// Connect to MongoDB
 connectDB();
 
-app.use(logger);
+// app.use(logger);
 
+// Middlewares
 app.use(cors(corsOptions));
-
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("Hey this is my API running!");
 });
 
+// Routes
 app.use("/auth", require("./routes/authRoute"));
-
 app.use("/users", require("./routes/usersRoute"));
-
 app.use("/articles", require("./routes/articleRoute"));
+app.use("/messages", require("./routes/messageRoute"));
+app.use("/forums", require("./routes/ForumRoute"));
 
+// Initialize Socket.io
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+// Handle Socket Connections
+socketHandler(io);
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Not found middleware
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
@@ -41,17 +62,19 @@ app.all("*", (req, res) => {
   }
 });
 
-app.use(errorHandler);
-
+// Start server
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
 
 mongoose.connection.on("error", (err) => {
   console.log(err);
-  logEvents(
-    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
+  server.close(() => {
+    process.exit(1);
+  });
+  // logEvents(
+  //   `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+  //   "mongoErrLog.log"
+  // );
 });
