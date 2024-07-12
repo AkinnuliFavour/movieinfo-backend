6c7a6563fe025e1031dbe16bfa16544727c7796c
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-07-12 20:05:52 +0100

    refactor: save admin status in cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,25 +6,38 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // Check if email and password is sent in request body
     if (!email || !password) {
       return res.json({ message: "All fields are required" });
     }
 
+    // Check if user exists
     const user = await User.findOne({ email });
 
+    // Return an error message if user does not exist
     if (!user) {
-      return res.json({ message: "Incorrect password or email" });
+      return res.json({ message: "User does not exist" });
     }
 
+    // Check if password is correct
     const auth = bcrypt.compare(password, user.password);
 
+    // Return an error message if password is not correct
     if (!auth) {
-      return res.json({ message: "Incorrect password or email" });
+      return res.json({ message: "Incorrect password" });
     }
 
+    // Create a jwt token
     const token = createSecretToken(user._id);
 
+    // Store token in cookie
     res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+
+    // Store admin status in cookie
+    res.cookie("admin", user.admin, {
       withCredentials: true,
       httpOnly: false,
     });
@@ -34,6 +47,7 @@ const login = async (req, res) => {
       .json({ message: "User logged in successfully", success: true });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error: "Error logging in" });
   }
 };
 
