82f52accf0587447a42df3a65674fb69333430fd
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-07-15 06:52:19 +0100

    refactor: create and export middleware function

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,7 +1,9 @@
-module.exports = (req, res, next) => {
+const isAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
     res.status(403).json({ error: "Access denied. Admin only." });
   }
 };
+
+module.exports = isAdmin;
